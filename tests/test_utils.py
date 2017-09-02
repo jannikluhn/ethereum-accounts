37a37a3625e5fea7da275b3c75c727992f6f4009
Jannik Luhn <jannik.luhn@brainbot.com>
2017-09-02 18:52:17 +0200

    Added tests for public_key_to_address

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,7 +6,7 @@ from ethereum.utils import (
 )
 
 from eth_utils import (
-    add_0x_prefix,
+    remove_0x_prefix,
     encode_hex,
     decode_hex,
     is_0x_prefixed,
@@ -19,6 +19,7 @@ from eth_accounts import (
     random_private_key,
     private_key_to_address,
     private_key_to_public_key,
+    public_key_to_address,
 )
 
 
@@ -39,7 +40,7 @@ def test_private_key_to_public_key(key):
     assert len(public_key) == 130 + 2
     assert public_key == reference
     assert private_key_to_public_key(decode_hex(key)) == reference
-    assert private_key_to_public_key(add_0x_prefix(key)) == reference
+    assert private_key_to_public_key(remove_0x_prefix(key)) == reference
 
 
 @pytest.mark.parametrize('key', [random_private_key() for _ in range(100)])
@@ -48,9 +49,20 @@ def test_private_key_to_address(key):
     reference = encode_hex(privtoaddr(decode_hex(key)))
     address = private_key_to_address(key)
     assert is_0x_prefixed(address)
-    assert is_hex(address)
-    assert len(address) == 40 + 2
+    assert is_checksum_address(address)
     assert is_same_address(address, reference)
     assert is_same_address(private_key_to_address(decode_hex(key)), reference)
-    assert is_same_address(private_key_to_address(add_0x_prefix(key)), reference)
+    assert is_same_address(private_key_to_address(remove_0x_prefix(key)), reference)
+
+
+@pytest.mark.parametrize('key', [random_private_key() for _ in range(100)])
+def test_public_key_to_address(key):
+    # tests against pyethereum
+    public_key = encode_hex(privtopub(decode_hex(key)))
+    reference = privtoaddr(decode_hex(key))
+    address = public_key_to_address(public_key)
+    assert is_0x_prefixed(address)
     assert is_checksum_address(address)
+    assert is_same_address(address, reference)
+    assert is_same_address(public_key_to_address(decode_hex(public_key)), reference)
+    assert is_same_address(public_key_to_address(remove_0x_prefix(public_key)), reference)
