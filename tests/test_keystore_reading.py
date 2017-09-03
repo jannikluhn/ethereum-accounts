d63ed5ae829347235695f63e32d6c0b9659c45e1
Jannik Luhn <jannik.luhn@brainbot.com>
2017-09-03 11:51:30 +0200

    Added test keystores exported from Parity and Geth

--- tests/test_keystore_reading.py
+++ tests/test_keystore_reading.py
@@ -24,6 +24,8 @@ testdata_directory = os.path.join(os.path.dirname(os.path.realpath(__file__)), '
 official_test_vector_path = os.path.join(testdata_directory, 'official_keystore_tests.json')
 pbkdf2_keystore_template_path = os.path.join(testdata_directory, 'pbkdf2_keystore_template.json')
 scrypt_keystore_template_path = os.path.join(testdata_directory, 'scrypt_keystore_template.json')
+parity_keystore_path = os.path.join(testdata_directory, 'parity_keystore.json')
+geth_keystore_path = os.path.join(testdata_directory, 'geth_keystore.json')
 
 
 def test_official_vectors():
@@ -37,6 +39,22 @@ def test_official_vectors():
         assert account.private_key == private_key
 
         assert account.exposed_address is None
+
+
+def test_parity():
+    account = Account.from_keystore(open(parity_keystore_path), b'password')
+    target_private_key = '0x2c535b74a0c4b339c669ba5b508b30c90801069b5bc5a1e0ae865e5c4f9e82cd'
+    assert account.private_key == target_private_key
+    assert account.address == '0xa82293681EA20394156c6b7B4773982Ccdf21aA4'
+    assert account.id == '3f9bc902-7b09-f848-d9b5-3d8f925e6df4'
+
+
+def test_geth():
+    account = Account.from_keystore(open(geth_keystore_path), b'password')
+    target_private_key = '0x13d39071f135b8a7a5f770172275d7400b3b181fa35227b722670800dd2b2ba9'
+    assert account.private_key == target_private_key
+    assert account.address == '0xbe43b4967B81e3a14C6CeCac2e1A89B4E681Cab5'
+    assert account.id == '02068fb6-1388-45e1-9071-b8a379fd5474'
 
 
 @pytest.fixture
